5df45b54834eb572e39c81db36d097105e7f56a6
Prateek Singh <prateek6394121233singh@gmail.com>
2025-02-04 00:42:43 +0530

    Delete python directory

--- python/lab3.2.py
+++
@@ -1,9 +0,0 @@
-x=int(input("enter the value of X "))
-y=int(input("enter the value of y"))
-z=int(input("enter the value of z"))
-
-if x==y and x==z :
-    print((x+y+x)*3)
-else:
-    print(x+y+z)
-    
--- python/lab3.3.py
+++
@@ -1,5 +0,0 @@
-x=int(input("enter the value for the x :"))
-if x%2==0:
-    print("the given number is even")
-else:
-   print("The given number is odd") 
